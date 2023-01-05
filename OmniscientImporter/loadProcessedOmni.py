5850978adf9a6189ddda3fe129be85d7e3ba9c56
Sightskeye <>
2023-01-05 20:02:29 +0100

    Refact: Separate func into other file

---
+++ OmniscientImporter/loadProcessedOmni.py
@@ -0,0 +1,23 @@
+import bpy
+import json
+import os
+from . import bl_info
+from .ui.info_popups import showTextPopup
+
+def loadProcessedOmni(self, video_filepath, camera_filepath, geo_filepath):
+    # Import the .abc file into the blender scene
+    bpy.ops.wm.alembic_import(filepath=camera_filepath)
+
+    # Import the .obj file into the blender scene
+    bpy.ops.import_scene.obj(filepath=geo_filepath)
+
+    # Import the .mov file into the blender scene
+    # -- RENDER --
+    cam = bpy.context.scene.objects['cameras']
+    bpy.context.scene.render.film_transparent = True
+    img = bpy.data.images.load(video_filepath)
+    cam.data.show_background_images = True
+    bg = cam.data.background_images.new()
+    bg.image = img
+
+    showTextPopup("Succes !")
--- OmniscientImporter/omniHandler.py
+++ OmniscientImporter/omniHandler.py
@@ -2,7 +2,7 @@ import bpy
 import json
 import os
 from . import bl_info
-from .ui.info_popups import showTextPopup
+from .loadProcessedOmni import loadProcessedOmni
 
 def loadOmni(self, omni_file):
     isVideoFileMissing = False
@@ -64,21 +64,3 @@ def loadOmni(self, omni_file):
             VideoPath=video_filepath,
             GeoPath=geo_filepath
         )
-
-def loadProcessedOmni(self, video_filepath, camera_filepath, geo_filepath):
-    # Import the .abc file into the blender scene
-    bpy.ops.wm.alembic_import(filepath=camera_filepath)
-
-    # Import the .obj file into the blender scene
-    bpy.ops.import_scene.obj(filepath=geo_filepath)
-
-    # Import the .mov file into the blender scene
-    # -- RENDER --
-    cam = bpy.context.scene.objects['cameras']
-    bpy.context.scene.render.film_transparent = True
-    img = bpy.data.images.load(video_filepath)
-    cam.data.show_background_images = True
-    bg = cam.data.background_images.new()
-    bg.image = img
-
-    showTextPopup("Succes !")
