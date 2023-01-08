c154be778c3ec831e9c1192f4974295481aeac18
Sightskeye <>
2023-01-08 11:47:12 +0100

    Fix: Video doesn't freeze after scene edit

--- OmniscientImporter/loadProcessedOmni.py
+++ OmniscientImporter/loadProcessedOmni.py
@@ -17,11 +17,13 @@ def loadProcessedOmni(video_filepath, camera_filepath, geo_filepath):
     bpy.context.scene.render.film_transparent = True
     img = bpy.data.images.load(video_filepath)
     width, height = img.size
+    frame_duration = img.frame_duration
     bpy.context.scene.render.resolution_x = width
     bpy.context.scene.render.resolution_y = height
     cam.data.show_background_images = True
     bg = cam.data.background_images.new()
     bg.image = img
     bg.image_user.frame_start = 1
+    bg.image_user.frame_duration = frame_duration
 
     showTextPopup("Succes !")
