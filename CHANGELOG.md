d1fca6388ef0e7dd6b44cfce06734b3f8a17e216
Spritzleb <>
2023-01-08 12:37:47 +0100

    Bump: v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,12 @@
+# 1.0.1 - (2023.01.08)
+
+### Fix
+- Import .omni without missing files
+- .omni import version check
+- No frame offset video start
+- 4:3 video display
+- Video doesn't freeze after scene edit
+
 # 1.0.0 - (2023.01.06)
 
 ### Features
--- OmniscientImporter/__init__.py
+++ OmniscientImporter/__init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "author" : "Stellaxis OÜ",
     "description" : "Import data recorded by the Omniscient iOS application.",
     "blender" : (2, 80, 0),
-    "version" : (1, 0, 0),
+    "version" : (1, 0, 1),
     "location" : "File > Import > Omniscient (.omni)",
     "warning" : "",
     "category" : "Import-Export"
