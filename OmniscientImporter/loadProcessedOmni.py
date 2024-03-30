4e27f3b30dd773acd283abf1ec2f9f1c1b5b096d
Sightskeye <>
2024-03-30 02:22:14 +0100

    Feat: Support fbx camera import

--- OmniscientImporter/loadProcessedOmni.py
+++ OmniscientImporter/loadProcessedOmni.py
@@ -32,6 +32,9 @@ def loadProcessedOmni(video_filepath, camera_filepath, geo_filepath):
     # .usd / .usdc / .usda
     if camera_filepath.endswith('.usd') or camera_filepath.endswith('.usdc') or camera_filepath.endswith('.usda'):
         bpy.ops.wm.usd_import(filepath=camera_filepath)
+    # .fbx
+    if camera_filepath.endswith('.fbx'):
+        bpy.ops.import_scene.fbx(filepath=camera_filepath)
 
     # Import the .mov file into the blender scene
     # -- RENDER --
