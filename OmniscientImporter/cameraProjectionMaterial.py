8db513751908ca13e27104d30675d46611cb3885
Sightskeye <>
2024-05-25 19:50:58 +1000

    Feat: Sensor and focal per camera projector

--- OmniscientImporter/cameraProjectionMaterial.py
+++ OmniscientImporter/cameraProjectionMaterial.py
@@ -83,16 +83,18 @@ def create_projection_shader(material_name, new_image_name, new_camera):
         if is_blender_4():
             node_group.interface.new_socket(name="Vector Input", in_out='INPUT')
             node_group.interface.new_socket(name="Vector Output", in_out='OUTPUT')
+            focal_length_socket = node_group.interface.new_socket(name="Focal Length", in_out='INPUT', socket_type='NodeSocketFloat',)
+            sensor_size_socket = node_group.interface.new_socket(name="Sensor Size", in_out='INPUT', socket_type='NodeSocketFloat',)
         else:
             node_group.inputs.new(name="Vector Input", type='NodeSocketVector')
             node_group.outputs.new(name="Vector Output", type='NodeSocketVector')
+            focal_length_socket = node_group.inputs.new(name="Focal Length", type='NodeSocketFloat')
+            sensor_size_socket = node_group.inputs.new(name="Sensor Size", type='NodeSocketFloat')
 
         separate_xyz_node = create_node_in_group(node_group, "ShaderNodeSeparateXYZ", "Separate XYZ", (-860.0, 100.0))
         combine_xyz_node = create_node_in_group(node_group, "ShaderNodeCombineXYZ", "Combine XYZ", (-40.0, 0.0))
         combine_xyz_node.inputs['Z'].default_value = 1.0
 
-        focal_length_node = create_node_in_group(node_group, "ShaderNodeValue", "Focal Length", (-880.0, -360.0), label='Focal Length')
-        sensor_size_node = create_node_in_group(node_group, "ShaderNodeValue", "Sensor Size", (-880.0, -460.0), label='Sensor Size')
         x_res_node = create_node_in_group(node_group, "ShaderNodeValue", "X Resolution", (-880.0, -160.0), label='X Resolution')
         y_res_node = create_node_in_group(node_group, "ShaderNodeValue", "Y Resolution", (-880.0, -260.0), label='Y Resolution')
 
@@ -124,16 +126,18 @@ def create_projection_shader(material_name, new_image_name, new_camera):
         create_link_in_group(node_group, math_divide_1, 0, math_multiply_1, 1)
         create_link_in_group(node_group, math_multiply_1, 0, combine_xyz_node, 1)
         create_link_in_group(node_group, combine_xyz_node, 0, vector_math_scale, "Vector")
-        create_link_in_group(node_group, focal_length_node, 0, math_divide_3, 0)
         create_link_in_group(node_group, math_divide_3, 0, vector_math_scale, "Scale")
         create_link_in_group(node_group, math_divide_4, 0, math_multiply_1, 0)
         create_link_in_group(node_group, math_multiply_2, 0, math_divide_2, 1)
-        create_link_in_group(node_group, sensor_size_node, 0, math_divide_3, 1)
         create_link_in_group(node_group, math_divide_2, 0, combine_xyz_node, 0)
         create_link_in_group(node_group, vector_math_scale, 0, vector_math_add, 0)
         create_link_in_group(node_group, vector_math_add, 0, group_output_node, 0)
         create_link_in_group(node_group, x_res_node, 0, math_divide_1, 0)
         create_link_in_group(node_group, y_res_node, 0, math_divide_1, 1)
+
+        # Link focal length and sensor size inputs
+        create_link_in_group(node_group, group_input_node, 1, math_divide_3, 0)  # Focal length
+        create_link_in_group(node_group, group_input_node, 2, math_divide_3, 1)  # Sensor size
 
         # Add drivers to the resolution nodes
         def add_driver(socket, data_path):
@@ -149,27 +153,42 @@ def create_projection_shader(material_name, new_image_name, new_camera):
         add_driver(x_res_node.outputs[0], "render.resolution_x")
         add_driver(y_res_node.outputs[0], "render.resolution_y")
 
-        # Add drivers to the focal length and sensor size nodes
-        def add_camera_driver(socket, camera, data_path):
-            driver = socket.driver_add("default_value").driver
-            driver.type = 'SCRIPTED'
-            var = driver.variables.new()
-            var.name = 'prop'
-            var.targets[0].id_type = 'OBJECT'
-            var.targets[0].id = camera
-            var.targets[0].data_path = data_path
-            driver.expression = var.name
-
-        if new_camera:
-            add_camera_driver(focal_length_node.outputs[0], new_camera, "data.lens")
-            add_camera_driver(sensor_size_node.outputs[0], new_camera, "data.sensor_width")
-
     # Create the node group in the new material
     camera_projector_group_node = nodes.new("ShaderNodeGroup")
     camera_projector_group_node.location = (-1000.0, -vertical_spacing * (len(existing_image_nodes) + 1))
     camera_projector_group_node.width = 166.7
     camera_projector_group_node.label = 'Camera Projector Omni'
     camera_projector_group_node.node_tree = node_group
+
+    # Add drivers to the inputs of the node group
+    def add_camera_driver(node, input_name, camera, data_path):
+        # Verify the input name is valid and the node has such an input
+        if input_name not in node.inputs:
+            print(f"Error: No input named {input_name} in node {node.name}")
+            return
+
+        input_socket = node.inputs[input_name]
+
+        # Attempt to add a driver to the input socket
+        try:
+            driver_fcurve = input_socket.driver_add("default_value")
+            driver = driver_fcurve.driver
+            driver.type = 'SCRIPTED'
+
+            var = driver.variables.new()
+            var.name = 'prop'
+            var.targets[0].id_type = 'OBJECT'
+            var.targets[0].id = camera
+            var.targets[0].data_path = data_path
+
+            driver.expression = var.name
+
+        except AttributeError as e:
+            print(f"Failed to add driver to {input_name} on node {node.name}: {e}")
+            
+    if new_camera:
+        add_camera_driver(camera_projector_group_node, "Focal Length", new_camera, "data.lens")  # Focal Length
+        add_camera_driver(camera_projector_group_node, "Sensor Size", new_camera, "data.sensor_width")  # Sensor Size
 
     # Create links in the main material
     def create_link(node_tree, from_node, from_socket, to_node, to_socket):
